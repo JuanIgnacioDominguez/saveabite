02494212f26bec7e70697e815ea85207dfe1486c
chrisca88 <118643501+chrisca88@users.noreply.github.com>
2024-06-19 00:45:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -348,6 +348,15 @@ def seleccionar_membresia_empresa(empresa_membresia):
     flash('Membresía seleccionada con éxito', 'success')
     return '', 204
 
+@app.route('/cancelar_membresia_empresa', methods=['POST'])
+def cancelar_membresia_empresa():
+    user_id = session.get('user_id')
+    conn = get_db_connection()
+    conn.execute('UPDATE usuarioEmpresa SET membresia = NULL WHERE id = ?', (user_id,))
+    conn.commit()
+    conn.close()
+    flash('Membresía de empresa dada de baja correctamente.', 'success')
+    return redirect(url_for('membresia_empresa'))
 
 
 
